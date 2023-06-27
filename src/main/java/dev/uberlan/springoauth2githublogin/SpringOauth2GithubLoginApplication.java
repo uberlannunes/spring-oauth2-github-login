f7380117e2203ce1c5701b2e324ce1f92d16af56
uberlannunes <uberlannunes@gmail.com>
2023-06-27 09:42:01 -0300

    first commit

---
+++ src/main/java/dev/uberlan/springoauth2githublogin/SpringOauth2GithubLoginApplication.java
@@ -0,0 +1,13 @@
+package dev.uberlan.springoauth2githublogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2GithubLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2GithubLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/uberlan/springoauth2githublogin/SpringOauth2GithubLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.uberlan.springoauth2githublogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2GithubLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
